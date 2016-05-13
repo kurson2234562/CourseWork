3c8c9705462d82e985abeffb6d95ee8eb80f2d14
kurson2234562 <kurson.aaa@yandex.ua>
2016-05-13 19:53:01 +0300

    Bug fixes. Before update working

--- 6/ChangeDatabase.Designer.cs
+++ 6/ChangeDatabase.Designer.cs
@@ -32,18 +32,30 @@
             this.MinimizeButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
             this.MaximizeButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
             this.audio_libDataSet = new _6.Audio_libDataSet();
             this.audiolibDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.songerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.songerTableAdapter = new _6.Audio_libDataSetTableAdapters.SongerTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.InsertButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.audio_libDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.audiolibDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.songerBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MinimizeButton
@@ -97,54 +109,48 @@
             this.MaximizeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.MaximizeButton.UseVisualStyleBackColor = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(387, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 27;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 228);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 249);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(552, 185);
             this.dataGridView1.TabIndex = 28;
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(66)))), ((int)(((byte)(98)))));
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Songer",
-            "Genre",
-            "Song",
             "Album_Info",
             "Composition_Album",
-            "Listening",
+            "Genre",
             "Groups",
-            "Participation"});
-            this.comboBox1.Location = new System.Drawing.Point(326, 421);
+            "Listening",
+            "Participation",
+            "Song",
+            "Songer"});
+            this.comboBox1.Location = new System.Drawing.Point(326, 442);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(238, 21);
             this.comboBox1.TabIndex = 29;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // button2
+            // ShowButton
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(66)))), ((int)(((byte)(98)))));
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(12, 419);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(203, 23);
-            this.button2.TabIndex = 30;
-            this.button2.Text = "Показать таблицу";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.ShowButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(66)))), ((int)(((byte)(98)))));
+            this.ShowButton.FlatAppearance.BorderSize = 0;
+            this.ShowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowButton.ForeColor = System.Drawing.Color.White;
+            this.ShowButton.Location = new System.Drawing.Point(12, 440);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(203, 23);
+            this.ShowButton.TabIndex = 30;
+            this.ShowButton.Text = "Показать таблицу";
+            this.ShowButton.UseVisualStyleBackColor = false;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
             // audio_libDataSet
             // 
@@ -165,16 +171,136 @@
             // 
             this.songerTableAdapter.ClearBeforeFill = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(13, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(13, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(13, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 37;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 38;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 39;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 40;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(94, 115);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 41;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.InsertButton);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(15, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 175);
+            this.groupBox1.TabIndex = 42;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление";
+            this.groupBox1.Visible = false;
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(66)))), ((int)(((byte)(98)))));
+            this.InsertButton.FlatAppearance.BorderSize = 0;
+            this.InsertButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InsertButton.Location = new System.Drawing.Point(16, 146);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(178, 23);
+            this.InsertButton.TabIndex = 42;
+            this.InsertButton.Text = "Добавить запись в БД";
+            this.InsertButton.UseVisualStyleBackColor = false;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
             // ChangeDatabase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(61)))));
-            this.ClientSize = new System.Drawing.Size(576, 454);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(576, 473);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ShowButton);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.MinimizeButton);
             this.Controls.Add(this.MaximizeButton);
             this.Controls.Add(this.CloseButton);
@@ -188,6 +314,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.audio_libDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.audiolibDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.songerBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -197,13 +325,25 @@
         private System.Windows.Forms.Button MinimizeButton;
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button MaximizeButton;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ShowButton;
         private System.Windows.Forms.BindingSource audiolibDataSetBindingSource;
         private Audio_libDataSet audio_libDataSet;
         private System.Windows.Forms.BindingSource songerBindingSource;
         private Audio_libDataSetTableAdapters.SongerTableAdapter songerTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button InsertButton;
     }
 }
--- 6/ChangeDatabase.cs
+++ 6/ChangeDatabase.cs
@@ -12,15 +12,32 @@ namespace _6
         DataTable dt;
         SqlConnection con = new SqlConnection();
         private int x = 0, y = 0;
+        void ClearComp()
+        {
+            label1.Text = null;
+            label2.Text = null;
+            label3.Text = null;
+            label4.Text = null;
+            label5.Text = null;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+            groupBox1.Visible = false;
+        }
         public ChangeDatabase()
         {
             InitializeComponent();
         }
 
-        private void ChangeDatabase_MouseDown(object sender, MouseEventArgs e)
+        private void ChangeDatabase_Load(object sender, EventArgs e)
         {
-            x = e.X;
-            y = e.Y;
+            ClearComp();
+            this.songerTableAdapter.Fill(this.audio_libDataSet.Songer);
+            con.ConnectionString = @"Data Source=.;Initial Catalog=Audio_lib; Integrated Security=true";
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            ShowButton.Enabled = false;
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
@@ -28,29 +45,9 @@ namespace _6
             this.Close();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (comboBox1.SelectedItem.ToString() != "")
-            {
-                sda = new SqlDataAdapter(@"Select * From " + comboBox1.SelectedItem.ToString(), con);
-                dt = new DataTable();
-                sda.Fill(dt);
-                dataGridView1.DataSource = dt;
-            }
-            dataGridView1.Width = dataGridView1.Columns.Count * dataGridView1.Columns[1].Width + 50;
-        }
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            button2.Enabled = true;
-        }
-
-        private void ChangeDatabase_Load(object sender, EventArgs e)
-        {
-            this.songerTableAdapter.Fill(this.audio_libDataSet.Songer);
-            con.ConnectionString = @"Data Source=.;Initial Catalog=Audio_lib; Integrated Security=true";
-            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            button2.Enabled = false;
+            ShowButton.Enabled = true;
         }
 
         private void ChangeDatabase_MouseMove(object sender, MouseEventArgs e)
@@ -59,6 +56,60 @@ namespace _6
             {
                 this.Location = new System.Drawing.Point(this.Location.X + (e.X - x), this.Location.Y + (e.Y - y));
             }
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() != "")
+            {
+                sda = new SqlDataAdapter(@"Select * From " + comboBox1.SelectedItem.ToString(), con);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                ClearComp();
+                int i = 0;
+                int cnt = dt.Columns.Count;
+                groupBox1.Visible = true;
+                foreach (Control c in groupBox1.Controls)
+                {
+                    if (i < cnt)
+                    {
+                        if (c is Label)
+                        {
+                            (c as Label).Text = dt.Columns[i].ToString();
+                            i++;
+                        }
+                    }
+                }
+                i = 0;
+                foreach (Control c in groupBox1.Controls)
+                {
+                    if (i < cnt)
+                    {
+                        if (c is TextBox)
+                        {
+                            (c as TextBox).Visible = true;
+                            i++;
+                        }
+                    }
+                }
+                dataGridView1.Width = dataGridView1.Columns.Count * dataGridView1.Columns[1].Width + 50;
+            }
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Insert Into " + comboBox1.SelectedItem.ToString() + "Values(" + textBox1.Text + "," + textBox2.Text + "," + textBox3.Text + "," + textBox4.Text + "," + textBox5.Text + ")");
+            sda = new SqlDataAdapter(@"Insert Into " + comboBox1.SelectedItem.ToString()+"Values("+textBox1.Text+"," + textBox2.Text + "," + textBox3.Text + "," + textBox4.Text + "," + textBox5.Text + ")", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void ChangeDatabase_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
         }
     }
 }
